012f79456e2014029e6173de1abee2322aa977a1
poliHorser <134537683+poliHorser@users.noreply.github.com>
2024-06-03 21:05:43 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ function App() {
         <BrowserRouter>
             <Routes>
                 <Route
-                    path="//vite-react-eslint-starter-template"
+                    path="/vite-react-eslint-starter-template"
                     element={
                         <Navigate to="/vite-react-eslint-starter-template/home" />
                     }
